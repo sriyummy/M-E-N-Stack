8b0a9d8930660d155eea29b64865a63d884c3460
sriyummy <sriyam2004@gmail.com>
2025-04-05 16:50:13 +0530

    Routing

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const catMe = require ('cat-me');
-console.log(catMe());
+const http = require ('http');
+
+const server = http.createServer ((req, res) => {
+    if (req.url == '/about') {
+        res.end ('The about page!');
+    }
+    else if (req.url == '/contact') {
+        res.end ('This is the contact page!');
+    }
+    else if (req.url == '/') {
+        res.end ('Welcome to the home page!');
+    }
+});
+
+server.listen(3000);
+console.log ('Server is running on port 3000');
