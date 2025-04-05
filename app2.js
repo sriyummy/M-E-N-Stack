a1b60f2851bdfcdcf8e3ea16129c1a349f53c0a3
sriyummy <sriyam2004@gmail.com>
2025-04-05 19:55:06 +0530

    User added

--- app2.js
+++ app2.js
@@ -26,6 +26,21 @@ app.get ('/profile', (req, res) => {
     res.send ('This is the profile page!');
 });
 
+app.get ('/register', (req, res) => {
+    res.render ('register');
+});
+
+app.post ('/register', async (req, res) => {
+    const {username, email, password} = req.body;
+
+    const newUser = await userModel.create ({
+        username: username,
+        email: email,
+        password: password
+    })
+    res.send (newUser);
+})
+
 app.post ('/get-form-data', (req, res) => {
     console.log (req.body);
     res.send ('Data Received!');
