d426f4b1e9596abc6e0baa44b96be782f55f0ea9
sriyummy <sriyam2004@gmail.com>
2025-04-05 18:29:41 +0530

    GET and POST forms

--- app2.js
+++ app2.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 
 app.use(morgan('dev'));;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -18,7 +20,12 @@ app.get ('/', (req, res, next) => {
 
 app.get ('/profile', (req, res) => {
     res.send ('This is the profile page!');
-})
+});
+
+app.post ('/get-form-data', (req, res) => {
+    console.log (req.body);
+    res.send ('Data Received!');
+});
 
 app.listen(3002);
 console.log ('Server running on port 3002');
