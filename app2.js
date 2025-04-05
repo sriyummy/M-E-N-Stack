b6e2bc7bf916b6c5166086d3608700d6a5ed8f9b
sriyummy <sriyam2004@gmail.com>
2025-04-05 17:44:11 +0530

    middleware - custom and imported

--- app2.js
+++ app2.js
@@ -1,13 +1,24 @@
+const morgan = require ('morgan');
 const express = require('express');
 const app = express();
 
-app.get ('/', (req, res) => {
-    res.send ('Hello World!')
-});
+app.use(morgan('dev'));;
+
+app.set('view engine', 'ejs');
+
+app.get ('/', (req, res, next) => {
+        const a = 5;
+        const b = 10;
+        console.log (a+b);
+
+        next();
+    }, (req, res) => {
+        res.render ('index');
+    });
 
 app.get ('/profile', (req, res) => {
     res.send ('This is the profile page!');
 })
 
-app.listen(3001);
-console.log ('Server running on port 3001');
+app.listen(3002);
+console.log ('Server running on port 3002');
