3afe74df46e8457001d620a17f6e42ef42c690a4
sriyummy <sriyam2004@gmail.com>
2025-04-05 18:55:06 +0530

    Frontend linking with backend forms

--- app2.js
+++ app2.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(morgan('dev'));;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
